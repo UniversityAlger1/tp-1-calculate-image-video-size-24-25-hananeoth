92510a69c889bc691b69bf320718b59436db71ba
hananeoth <148763025+hananeoth@users.noreply.github.com>
2024-11-17 22:37:00 +0200

    Update grayScaleImage.c

--- grayScaleImage.c
+++ grayScaleImage.c
@@ -6,7 +6,18 @@
 //   grayScale size of the image Bitmap (in byte)
 float grayScaleImage(int w, int h) {
    // YOUR CODE HERE - BEGIN
-
+   int size = w * h;
+  printf("La taille de l'image bitmap est : %d octets\n", size);
    // YOUR CODE HERE - END
-   return 0;
+    grayScaleImage(w , h);
+   return  size;
+}
+int main() {
+    int width = 640;  // Exemple de largeur
+    int height = 480; // Exemple de hauteur
+
+    // Appel de la fonction
+    float taille = grayScaleImage(width, height);
+    printf("Taille retournée : %.2f octets\n", taille);
+  return 0;
 }
